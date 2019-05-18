25d78d3e5ddd174f6ee991c3eb097bf29650c641
MuliTu <muli.turiel@gmail.com>
2019-05-18 17:00:40 +0300

    Updates

---
+++ precache-manifest.d15feee33070ed0d204770afd5fa14b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0771423156ab10c905e80b0f9a1d8e",
+    "url": "/Neuron-Graph/index.html"
+  },
+  {
+    "revision": "9a5c63a6a83d3613ab69",
+    "url": "/Neuron-Graph/static/css/main.35178b21.chunk.css"
+  },
+  {
+    "revision": "c4fa7fdeeb20c58397ac",
+    "url": "/Neuron-Graph/static/js/2.09702a68.chunk.js"
+  },
+  {
+    "revision": "9a5c63a6a83d3613ab69",
+    "url": "/Neuron-Graph/static/js/main.b2c03be1.chunk.js"
+  },
+  {
+    "revision": "2e3b85a14b1008701915",
+    "url": "/Neuron-Graph/static/js/runtime~main.28199534.js"
+  }
+]);
